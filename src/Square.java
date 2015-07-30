fc755b8222ea8a3de66d3fbe8f775d6e2d1be653
pkrivka <krivka.patrik@gmail.com>
2015-07-30 15:54:48 +0200

    change
    vykreslenie stvorca pomocou "line"

--- src/Square.java
+++ src/Square.java
@@ -24,22 +24,14 @@ public class Square extends Shape {
 	
 	
 	public String toString() {
-		//Line l=new Line(0, 0, this.getFill(), this.size, false);
+		Line l=new Line(this.x, 0, this.getFill(), this.size, false);
 		String square = "";
-		//odriadkuje pomocou zadanej pozicie y
 		for (int i=0;i<y;i++){
-			System.out.print("\n");
-		}		
-		for (int i = 0; i < size; i++) {
-			//vypise medzery + posunie vpravo podla zadanej premennej x
-			for (int k=0;k<x;k++){
-				square += " ";
-			}
-			for (int j=0;j<size;j++){
-				square += this.getFill();
-			}			
-			square += "\n";			
+			square+="\n";
 		}
+		for (int a = 0; a < this.size; a++) {
+			square += l+"\n";
+		}		
 		return square;
 	}
 
